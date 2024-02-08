f015194fa9f4c864c20513a6156f463851afaae6
Syed Abdul Rahman <56169702+Syed-SAR@users.noreply.github.com>
2024-02-09 01:45:18 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello, from docker');
+}
+);
+
+
+app.listen(8080, () => {
+    console.log('Listening on port 8080')
+}
+);
